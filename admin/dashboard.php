932bc206c8dd5bbae69691f5c212b5a011893933
jhonreimons90@gmail.com <jhonreimons90@gmail.com>
2022-06-16 23:43:05 +0700

    update data

---
+++ admin/dashboard.php
@@ -0,0 +1,59 @@
+<aside id="left-panel" class="left-panel">
+          <nav class="navbar navbar-expand-sm navbar-default">
+               <div id="main-menu" class="main-menu collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                         <li class="active">
+                              <a href="../admin"><i class="menu-icon fa fa-laptop"></i>Dashboard </a>
+                         </li>
+                         <li class="menu-title">Data Mahasiswa </li><!-- /.menu-title -->
+                         <li>
+                              <a href="data_mahasiswa.php"> <i class="menu-icon fa fa-user-md"></i>Data mahasiswa</a>
+                         </li>
+                         <li>
+                              <a href="data_mbkm.php"> <i class="menu-icon fa fa-user-md"></i>MBKM</a>
+                         </li>
+                         <li class="menu-title">Data Dosen</li><!-- /.menu-title -->
+                         <li>
+                              <a href="data_dosen.php"> <i class="menu-icon ti-user"></i>Data dosen</a>
+                         </li>
+                         <li class="data-toggle">
+                              <a href="tugas_akhir.php" class=" data-toggle" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Tugas Akhir</a>
+                         </li>
+                         <li class="menu-item-has-children dropdown">
+                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Proyek Akhir</a>
+                              <ul class="sub-menu children dropdown-menu">
+                                   <li><i class="fa fa-table"></i><a href="proyek_akhir_1.php">Proyek Akhir I</a></li>
+                                   <li><i class="fa fa-table"></i><a href="proyek_akhir_2.php">Proyek Akhir II</a></li>
+                              </ul>
+                         </li>
+                         <li>
+                              <a href="data_publikasi_dosen.php"> <i class="menu-icon fa fa-area-chart"></i>Publikasi Dosen</a>
+                         </li>
+                         <li class="menu-title">Data Kegiatan</li>
+                         <li>
+                              <a href="jadwal_kegiatan.php"> <i class="menu-icon fa fa-bar-chart"></i>Jadwal Kegiatan</a>
+                         </li>
+
+                         <li>
+                              <a href="data_penelitian_dosen.php"> <i class="menu-icon fa fa-area-chart"></i>Penelitian Dosen</a>
+                         </li>
+                         <li>
+                              <a href="penugasan_dosen.php"> <i class="menu-icon fa fa-area-chart"></i>Penugasan Dosen</a>
+                         </li>
+                         <li class="menu-title">HIMPUNAN MAHASISWA</li>
+                         <li>
+                              <a href="visi_misi.php"> <i class="menu-icon fa fa-bar-chart"></i>Visi Misi</a>
+                         </li>
+                         <li>
+                              <a href="bph_himpunan.php"> <i class="menu-icon fa fa-bar-chart"></i>BPH Himpunan</a>
+                         </li>
+                         <li>
+                              <a href="divisi_himpunan.php"> <i class="menu-icon fa fa-bar-chart"></i>Divisi Himpunan</a>
+                         </li>
+                         <li>
+                              <a href="kpu_himpunan.php"> <i class="menu-icon fa fa-bar-chart"></i>KPU Himpunan</a>
+                         </li>
+                    </ul>
+               </div><!-- /.navbar-collapse -->
+          </nav>
+     </aside>
